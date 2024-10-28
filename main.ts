0a02c504ab0a71465a0e382c8869c6525c06be53
IvanGuell <146827049+IvanGuell@users.noreply.github.com>
2024-10-28 17:05:47 +0100

    final

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
     music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.UntilDone)
 })
-input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
+input.onGesture(Gesture.TiltRight, function () {
     music.changeTempoBy(-20)
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+input.onGesture(Gesture.Shake, function () {
     music.setTempo(120)
 })
-input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
+input.onGesture(Gesture.TiltLeft, function () {
     music.changeTempoBy(20)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
+input.onButtonPressed(Button.B, function () {
     music.play(music.stringPlayable("D G B C E F A C5 ", 120), music.PlaybackMode.UntilDone)
 })
